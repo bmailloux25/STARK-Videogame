54b2ad9c00bdcad8ef46723d2295db36284e4053
Benjy <bmailloux25@gmail.com>
2020-10-17 12:03:25 -0500

    Added movement animation
    Character movement should be complete now

--- STARK Videogame/Assets/Player_Movement.cs
+++ STARK Videogame/Assets/Player_Movement.cs
@@ -7,12 +7,17 @@ public class Player_Movement : MonoBehaviour
     public float moveSpeed = 5f;
     public Rigidbody2D Rb;
     Vector2 movement;   // Store x,y
+    public Animator animator;
 
     // Update is called once per frame
     void Update()   // Input
     {
        movement.x = Input.GetAxisRaw("Horizontal");
        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     void FixedUpdate()  // Movement
