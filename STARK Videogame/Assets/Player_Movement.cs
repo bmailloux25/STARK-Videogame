8301ff9b62119e8db542a9924279c4386af8618c
Benjy <bmailloux25@gmail.com>
2020-10-16 19:15:43 -0500

    Added player movement script
    I added simple player movement. I will add animation later.

---
+++ STARK Videogame/Assets/Player_Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D Rb;
+    Vector2 movement;   // Store x,y
+
+    // Update is called once per frame
+    void Update()   // Input
+    {
+       movement.x = Input.GetAxisRaw("Horizontal");
+       movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()  // Movement
+    {
+        Rb.MovePosition(Rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
