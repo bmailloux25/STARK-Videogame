2b51d67ba55dd0997a3629ef549967ce2ae1b611
Benjy <bmailloux25@gmail.com>
2020-11-01 12:40:22 -0600

    Added enemy movement
    Added a basic script so the enemies will slowly move towards the player

---
+++ STARK Videogame/Assets/Enemy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Player player;
+    private float moveSpeed;
+    private Vector3 directionToPlayer;
+    private Vector3 localScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = FindObjectOfType(typeof(Player)) as Player;
+        moveSpeed = 2f;
+        localScale = transform.localScale;
+    }
+
+    private void FixedUpdate()
+    {
+        moveCharacter();
+    }
+
+    private void moveCharacter()
+    {
+        directionToPlayer = (player.transform.position - transform.position).normalized;
+        rb.velocity = new Vector2(directionToPlayer.x, directionToPlayer.y) * moveSpeed;
+    }
+
+    private void LateUpdate()
+    {
+        if (rb.velocity.x > 0)
+        {
+            transform.localScale = new Vector3(localScale.x, localScale.y, localScale.z);
+        }
+        else if (rb.velocity.x < 0)
+        {
+            transform.localScale = new Vector3(-localScale.x, localScale.y, localScale.z);
+        }
+    }
+}
